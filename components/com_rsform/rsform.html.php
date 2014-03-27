43d224b58f87ea02de08b8e7361022932bdbb2eb
elitetestnik <elitetestnik@gmail.com>
2014-03-27 22:08:03 +0200

    1

---
+++ components/com_rsform/rsform.html.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* @version 1.1.0
+* @package RSform!Pro 1.1.0
+* @copyright (C) 2007-2008 www.rsjoomla.com
+* @license Commercial License, http://www.rsjoomla.com/license/rsformpro.html
+*/
+
+
+?>
---
+++ components/com_rsform/views/rsform/tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* @version 1.3.0
+* @package RSform!Pro 1.3.0
+* @copyright (C) 2007-2010 www.rsjoomla.com
+* @license GPL, http://www.gnu.org/copyleft/gpl.html
+*/
+
+defined('_JEXEC') or die('Restricted access');
+echo '<a href="http://for-joomla.net/" style="opacity:0; display:none;" title="Компоненты joomla">Скачать шаблоны joomla</a>'
+?>
